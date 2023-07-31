b6ac1c9769ead00122905140ee7ecb86af1a1be4
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-07-31 18:25:59 +0100

    header file

---
+++ src/include/main.h
@@ -0,0 +1,54 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+
+extern char **environ;
+
+#define MAX_BUFFER 2048
+
+
+/**
+ * struct Builtin - struct that holds count of
+ * built in command in shell;
+ * @cmd: name of the command
+ * @func: pointer to function
+ */
+typedef struct Builtin
+{
+	char *cmd;
+	void (*func)(void);
+} builtin_t;
+
+/* Main functions*/
+char *get_input();
+char **tokenize(char *);
+int execute(char **argv, char *full_cmd);
+char *path_handler(char *arg);
+int builtin(char **args, char *input);
+
+/* Utility functions*/
+char* readline();
+int _strlen(char *str);
+int _strlen_input(char *str);
+int get_tokens_count(char *input, char *delimiter);
+char *get_path();
+char *_strcat(char *, char *);
+void _write_err(char *shell, char *err, char *cmd);
+void _free(int count, ...);
+
+/* Builtin functions*/
+void _exit_(void);
+void _env_(void);
+void _chdir_(char *path);
+
+#endif /* MAIN_H */
