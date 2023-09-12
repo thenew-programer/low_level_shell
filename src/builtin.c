621420a532b94d231f4ff63bc052fadb76035a1f
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-09-12 09:38:38 +0100

    add builtin function called history

--- src/builtin.c
+++ src/builtin.c
@@ -15,31 +15,31 @@
 int builtin(char **args, char *input) {
   int i = 0;
   builtin_t builtin[] = {
-      {"exit", _exit_},
-      {"env", _env_},
-      {"cd", NULL},
-      {NULL, NULL},
+		{"exit", _exit_},
+		{"env", _env_},
+		{"cd", NULL},
+		{"history", _history_},
+		{NULL, NULL},
   };
 
-  while (builtin[i].cmd) {
-    if (strcmp(builtin[i].cmd, args[0]) == 0) {
-      if (strcmp(builtin[i].cmd, "exit") == 0) {
-        _free(2, args, input);
-		exit(EXIT_SUCCESS);
-      } else if (strcmp(builtin[i].cmd, "cd") == 0) {
-        if (args[1] == NULL) {
-          _chdir_("/home/jos");
-          return (SUCCESS);
-        }
-        _chdir_(args[1]);
-        return (SUCCESS);
-      } else {
-
-        builtin[i].func();
-        return (SUCCESS);
-      }
-    }
-    i++;
-  }
+	while (builtin[i].cmd) {
+		if (strcmp(builtin[i].cmd, args[0]) == 0) {
+			if (strcmp(builtin[i].cmd, "exit") == 0) {
+				_free(2, args, input);
+				exit(EXIT_SUCCESS);
+			} else if (strcmp(builtin[i].cmd, "cd") == 0) {
+				if (args[1] == NULL) {
+				_chdir_("/home/jos");
+				return (SUCCESS);
+				}
+				_chdir_(args[1]);
+				return (SUCCESS);
+			}else {
+			builtin[i].func();
+			return (SUCCESS);
+			}
+		}
+		i++;
+	}
   return (FAILURE);
 }
